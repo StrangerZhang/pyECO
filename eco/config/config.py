eb75526892ea4af660ad938d2f00e59ad01e351d
StrangerZhang <40649426+StrangerZhang@users.noreply.github.com>
2018-10-06 18:36:11 +0800

    Update config.py

--- eco/config/config.py
+++ eco/config/config.py
@@ -3,5 +3,5 @@ from .otb_deep_config import OTBDeepConfig
 from .otb_hc_config import OTBHCConfig
 
 # config = DemoConfig()
-config = OTBDeepConfig()
-# config = OTBHCConfig()
+# config = OTBDeepConfig()
+config = OTBHCConfig()
